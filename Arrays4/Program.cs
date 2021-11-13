4d98db1617f3250fc79ec8c7885b49f0362d8367
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2021-11-13 14:06:29 +0300

    Diizi default değer atama...

---
+++ Arrays4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Arrays4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // eğer dizi elemanlarınız sabitse, dizinin başlangıç aşamısnda değer ataması yapabilirsiniz...
+            // sabit dizi
+
+            string[] mevsimler = { "İlkbahar", "Yaz", "Sonbahar", "kış" };
+            mevsimler[3] = "Kış";
+            char[] chars = { 'E', 'k', 'r', 'e', 'm' };
+            bool[] bools = { true, false };
+
+            // yukarıdaki tanımlamanın adı object initializer diye geçer...
+            // ekrana mevsimleri tek tek yazdıralım..
+            // Not : eğer blok içerisinde tek satır işlem yapacaksınız scope açıp kapamanıza gerek yoktur...
+            foreach (var item in mevsimler)
+                Console.Write($"{item}  ");
+
+            Console.WriteLine("********");
+
+            // ekrana mevsimleri tersten yazdıralım...
+            for (int i = mevsimler.Length - 1; i >= 0; i--)
+                Console.Write($"{mevsimler[i]}  ");
+
+            Console.ReadKey();
+        }
+    }
+}

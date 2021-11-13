b16392152d0c40b5b1ad08800b6e252b47242e55
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2021-11-13 14:04:23 +0300

    array örnek1...

---
+++ ArrayOrnek/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ArrayOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BT Akademi Öğrenci Kayıt Sistemi");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
